0915c093e99bdd6f28cc4bd46c55190fb33ec24e
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-13 00:16:57 -0300

    feat: add header to listing page

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -1,7 +1,14 @@
+import Header from '../components/Header'
+
 const Listing = () => (
-  <h1 className='text-3xl font-bold underline text-center mt-10'>
-    Listing
-  </h1>
+  <>
+    <Header />
+    <main className='bg-zinc-800 h-screen py-10 mt-10'>
+      <h1 className='text-3xl text-white font-bold text-center'>
+        Listing
+      </h1>
+    </main>
+  </>
 )
 
 export default Listing
