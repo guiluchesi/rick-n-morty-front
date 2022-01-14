1fa0c44c6d5674e0271e9bc86feb4f14cc64fdb1
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-13 23:19:19 -0300

    feat: tailwind container configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '0'
+      }
+    },
     extend: {}
   },
   plugins: []
