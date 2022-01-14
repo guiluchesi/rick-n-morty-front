f1590ddfc360bc43bde31648d00e1662733071fd
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-14 00:03:29 -0300

    feat: create text with status component

---
+++ src/components/TextWithStatus.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from 'react'
+
+const TextWithStatus = ({ text = '', status = 'Unknown' }): ReactElement => {
+  const statusMapping = new Map([
+    ['Unknown', 'before:bg-slate-400'],
+    ['Dead', 'before:bg-red-400'],
+    ['Alive', 'before:bg-green-500']
+  ])
+
+  const getStatusClass = (status: string): string => {
+    return statusMapping.get(status) ?? 'before:bg-slate-400'
+  }
+
+  return (
+    <div className={`pl-4 relative text-white before:content-[''] before:left-0 before:top-2 before:w-2 before:h-2 before:absolute before:rounded-full ${getStatusClass(status)}`}>
+      {status} - {text}
+    </div>
+  )
+}
+
+export default TextWithStatus
