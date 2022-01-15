fcc4b6236227e589d6740a86c0e2ff80df3b0ee4
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-15 00:29:14 -0300

    feat: move characters array to data folder

---
+++ src/data/characters.js
@@ -0,0 +1,27 @@
+const characters = [
+  {
+    id: 1,
+    name: 'Blue Diplomat',
+    status: 'Alive',
+    species: 'Alien',
+    location: 'Interdimensional Cable',
+    image: 'https://rickandmortyapi.com/api/character/avatar/51.jpeg',
+    episodes: [
+      'Interdimensional Cable 2: Tempting Fate'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Boobloosian',
+    status: 'Dead',
+    species: 'Alien',
+    location: 'Nuptia 4',
+    image: 'https://rickandmortyapi.com/api/character/avatar/55.jpeg',
+    episodes: [
+      'Big Trouble in Little Sanchez',
+      'The Wedding Squanchers'
+    ]
+  }
+]
+
+export default characters
