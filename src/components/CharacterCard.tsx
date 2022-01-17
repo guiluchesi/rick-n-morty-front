67cb1529932fba9df612361b6ab0e42f48d90e6e
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-16 22:46:44 -0300

    fix: card style adjustments

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -10,19 +10,19 @@ interface Props {
 const CharacterCard = ({ character }: Props): ReactElement => {
   return (
     <div className='card flex bg-zinc-700 rounded-lg overflow-hidden'>
-      <figure className='w-2/5'>
+      <figure className='max-w-[180px]'>
         <img className='object-cover h-full' src={character.image} alt={character.name} />
       </figure>
 
-      <div className='card-body px-4'>
-        <h3 className='text-3xl font-bold text-white mt-3'>{character.name}</h3>
+      <div className='card-body p-4'>
+        <h3 className='text-3xl font-bold text-white'>{character.name}</h3>
         <TextWithStatus status={character.status} text={character.species} />
 
         <p className='mt-5 text-neutral-400 font-light'>Last known location:</p>
-        <p className='text-white text-lg font-light'>{character.location}</p>
+        <p className='text-white text-lg font-light truncate'>{character.location}</p>
 
         <p className='mt-4 text-neutral-400 font-light'>First seen in:</p>
-        <p className='text-white font-light'>{character.episodes.at(0)}</p>
+        <p className='text-white font-light truncate'>{character.episodes.at(0)}</p>
       </div>
     </div>
   )
