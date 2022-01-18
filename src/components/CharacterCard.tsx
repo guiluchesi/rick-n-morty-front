783f4df110b9f03529fa5f4da6f5e4998bfa0099
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-01-17 23:49:47 -0300

    feat: styles adjustments

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,28 +1,28 @@
 import { ReactElement } from 'react'
 
 import TextWithStatus from './TextWithStatus'
-import { Character } from '../types'
+import { Character as CharacterType } from '../types'
 
 interface Props {
-  character: Character
+  character: CharacterType
 }
 
 const CharacterCard = ({ character }: Props): ReactElement => {
   return (
     <div className='card flex bg-zinc-700 rounded-lg overflow-hidden'>
-      <figure className='max-w-[180px]'>
+      <figure className='max-w-[160px] md:max-w-[220px]'>
         <img className='object-cover h-full' src={character.image} alt={character.name} />
       </figure>
 
       <div className='card-body p-4'>
-        <h2 className='text-3xl font-bold text-white'>{character.name}</h2>
+        <h2 className='text-2xl md:text-3xl font-bold text-white'>{character.name}</h2>
         <TextWithStatus status={character.status} text={character.species} />
 
         <p className='mt-5 text-neutral-400 font-light'>Last known location:</p>
         <p className='text-white text-lg font-light truncate'>{character.location}</p>
 
         <p className='mt-4 text-neutral-400 font-light'>First seen in:</p>
-        <p className='text-white font-light truncate'>{character.episodes.at(0)}</p>
+        <p className='text-white font-light truncate'>{character.episodes.at(0)?.name}</p>
       </div>
     </div>
   )
--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -4,22 +4,24 @@ import { Link } from 'react-router-dom'
 import Header from '../components/Header'
 import CharacterCard from '../components/CharacterCard'
 import characters from '../data/characters'
-import { Character } from '../types'
+import { Character as CharacterType } from '../types'
 
 const Listing = (): ReactElement => {
   return (
     <>
       <Header />
-      <main className='bg-zinc-800 h-screen py-10 mt-10'>
+      <main className='bg-zinc-800 py-10'>
         <h1 className='text-3xl text-white font-bold text-center'>
           Listing
         </h1>
-        <div className='mt-5 list container grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4'>
-          {characters.map((character: Character): ReactElement => (
-            <Link key={character.id} to={`/character/${character.id}`}>
-              <CharacterCard character={character} />
-            </Link>
-          ))}
+        <div className='container'>
+          <div className='mt-5 list mx-auto max-w-3xl xl:max-w-none grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4'>
+            {characters.map((character: CharacterType): ReactElement => (
+              <Link key={character.id} to={`/character/${character.id}`}>
+                <CharacterCard character={character} />
+              </Link>
+            ))}
+          </div>
         </div>
       </main>
     </>
